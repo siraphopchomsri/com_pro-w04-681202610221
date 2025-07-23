f81481955effc5bd02bdd2ce077bd8e8457a6c94
siraphopchomsri <jaychom92@gmail.com>
2025-07-23 14:47:47 +0700

    Update coding_w04-05-1.c

--- coding_w04-05-1.c
+++ coding_w04-05-1.c
@@ -1,11 +1,15 @@
-//กรณีที่ 2: ประกาศตัวแปร score เป็นชนิด float และกำหนดค่า 90.5
+#include <stdio.h>
 
-#include <stdio.h> // เรียกใช้ไลบรารีมาตรฐานสำหรับฟังก์ชัน Input/Output
+void countCall() {
+    static int counter = 0; // Static variable
+    counter++;
+    printf("Call Function: Counter = %d\n", counter);
+}
 
 int main() {
-    float score = 90.5f; // ประกาศตัวแปร score เป็นชนิด float (ทศนิยม) และกำหนดค่า 90.5
-                         // ใช้ 'f' ต่อท้าย 90.5 เพื่อระบุว่าเป็น float literal
-    printf("float score = 90.5 %d\n" , score); // แสดงผลตัวแปร score
-    printf(" score: %.1f\n", score); // แสดงผลตัวแปร score โดยให้แสดงทศนิยม 1 ตำแหน่ง
-    return 0; // คืนค่า 0 เมื่อโปรแกรมทำงานเสร็จสมบูรณ์
+    printf("Starting function calls...\n");
+    countCall();
+    printf("After first call:\n");
+    countCall();
+    return 0;
 }
