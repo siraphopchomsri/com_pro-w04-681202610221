34ede721769365d80c86680de1123d6192157c9b
siraphopchomsri <jaychom92@gmail.com>
2025-07-23 23:06:57 +0700

    Update coding_w04-03.c

--- coding_w04-03.c
+++ coding_w04-03.c
@@ -1,7 +1,7 @@
 #include <stdio.h> // สำหรับใช้ฟังก์ชัน printf()
 
 int main() { // ประกาศตัวแปรและกำหนดค่าเริ่มต้น
-    char nameInitial = 'S'; // Data Type: char (สำหรับเก็บอักขระเดี่ยว)
+    char nameInitial = 'J'; // Data Type: char (สำหรับเก็บอักขระเดี่ยว)
     int age = 18;           // Data Type: int (สำหรับเก็บจำนวนเต็ม)
     float weight = 60.5f;   // Data Type: float (สำหรับเก็บเลขทศนิยม, ใช้ 'f' ต่อท้ายเพื่อระบุว่าเป็น float literal)
     char gender[] = "Male"; // Data Type: char array (สำหรับเก็บข้อความ, ใน C คืออาร์เรย์ของ char)
